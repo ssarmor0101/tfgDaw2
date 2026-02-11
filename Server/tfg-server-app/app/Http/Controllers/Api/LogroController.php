a40a132447dfe39e8404ec4e369f388d7a44ecc1
ssarmor0101 <ssarmor0101@g.educaand.es>
2026-02-11 21:17:49 +0100

    Implementado LogroResource

--- Server/tfg-server-app/app/Http/Controllers/Api/LogroController.php
+++ Server/tfg-server-app/app/Http/Controllers/Api/LogroController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreLogroRequest;
+use App\Http\Resources\LogroResource;
 use App\Models\Logro;
 use Illuminate\Http\Request;
 
@@ -14,8 +15,14 @@ class LogroController extends Controller
      */
     public function index()
     {
-        $logros = Logro::all();
-        return $logros->toJson();
+        // $logros = Logro::all();
+        $logros = Logro::with('juego')->with('resultados')->get();
+        // $logros = Logro::paginate(5);
+        // return $logros;
+        // return $logros->toJson();
+        return response()->json($logros, 200);
+        // return response()->json(LogroResource::collection($logros), 200); // Faltan metadatos
+        // return LogroResource::collection($logros);
     }
 
     /**
@@ -27,7 +34,8 @@ class LogroController extends Controller
 
         $logro = Logro::create($validate);
 
-        return $logro->toJson();
+        // return $logro->toJson();
+        return response()->json(['message' => 'Logro creado con exito'], 201);
     }
 
     /**
@@ -35,7 +43,7 @@ class LogroController extends Controller
      */
     public function show(Logro $logro)
     {
-        //
+        return response()->json($logro, 200);
     }
 
     /**
---
+++ Server/tfg-server-app/app/Http/Resources/LogroResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LogroResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'juego_id' => $this->juego_id,
+            'name' => $this->name,
+            'descr' => $this->description,
+            'especial' => $this->juego->name . ' - ' . $this->name,
+        ];
+    }
+}
